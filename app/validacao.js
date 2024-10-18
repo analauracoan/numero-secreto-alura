2a7691ec10cef05da89a11435e001cbd111f087d
analauracoan <anacoan1@gmail.com>
2024-10-18 15:14:29 -0300

    feat(NumberValidation): add the main validation function to know if the number is correct

--- app/validacao.js
+++ app/validacao.js
@@ -3,10 +3,19 @@ function verificaSeChutePossuiValorValido(chute) {
 
   if (chuteForInvalido(numero)) {
     elementoChute.innerHTML += "<div>Valor inválido</div>";
+    return;
   }
 
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
     elementoChute.innerHTML += `Valor inválido: fale um número entre ${menorValor} e ${maiorValor}`;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era </h3>
+    `;
   }
 }
 
