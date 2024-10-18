f50ffc77fe50320f2f57453ce266f312cf76fa34
analauracoan <anacoan1@gmail.com>
2024-10-18 15:24:47 -0300

    feat(VoiceRecognition): add possibility to do multiple kicks and add tips

--- app/validacao.js
+++ app/validacao.js
@@ -14,8 +14,12 @@ function verificaSeChutePossuiValorValido(chute) {
   if (numero === numeroSecreto) {
     document.body.innerHTML = `
         <h2>Você acertou!</h2>
-        <h3>O número secreto era </h3>
+        <h3>O número secreto era ${numero}</h3>
     `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
+  } else if (numero < numeroSecreto) {
+    elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
   }
 }
 
